43ecddb2e9d06ab05785efdf200d2c117ca5f3d4
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-05-15 09:27:42 -0400

    t~.i~t~: def iter_to_str for custom stringification of iterables

--- tools/iter_tools.py
+++ tools/iter_tools.py
@@ -110,3 +110,145 @@ def inherit(
     }
 
     return result
+
+
+############################################################
+#   Stringifying
+############################################################
+
+ty__iter_to_str__style = ty.Mapping[str, ty.Any]
+ty__iter_to_str__style__to_str = ty.Callable[[ty.Iterable, ty.Any, int], str]
+
+ITER_TO_STR__STYLE_FAMILY = {
+    "base": {
+        "extends": None,
+        "to_str": (lambda i, x, c: repr(x)),
+        "before_all": "",
+        "after_all": "",
+        "between": ", ",
+        "before_each": "",
+        "after_each": "",
+    },
+    "plain": {
+        "extends": "base",
+        "to_str": (lambda i, x, c: str(x)),
+    },
+    "tuple": {
+        "extends": "base",
+        "before_all": "(",
+        "after_all": ")",
+    },
+    "list": {
+        "extends": "base",
+        "before_all": "[",
+        "after_all": "]",
+    },
+    "set": {
+        "extends": "base",
+        "before_all": "{",
+        "after_all": "}",
+    },
+    "dict": {
+        "extends": "set",
+        "to_str": (lambda i, x, c: f"{repr(x)}: {repr(c[x])}"),
+    },
+    "args": {
+        "extends": "base",
+    },
+    "kwargs": {
+        "extends": "args",
+        "to_str": (lambda i, x, c: f"{str(x)}={repr(c[x])}"),
+    },
+}
+
+ITER_TO_STR__STYLES = {
+    key: inherit(ITER_TO_STR__STYLE_FAMILY, key, delete_extends_key=True)
+    for key in ITER_TO_STR__STYLE_FAMILY.keys()
+}
+
+
+def iter_to_str(
+    c: ty.Iterable[ty.Any],
+    style: ty.Union[None, str, ty__iter_to_str__style] = None,
+    **options,
+) -> str:
+
+    # Parse `style`
+
+    style_name = None
+    style_dict = None
+
+    if style is None:
+
+        style_name = "base"
+
+    elif isinstance(style, str):
+        if style in ITER_TO_STR__STYLES:
+
+            style_name = style
+
+        else:
+            raise Exception("iter_to_str.UnknownStyleError")
+
+    elif isinstance(style, ty.Mapping):
+
+        style_dict = style
+
+    else:
+        raise Exception("iter_to_str.InvalidStyleError")
+
+    # Check `options` for valid `to_str`
+
+    if "to_str" in options:
+        if not isinstance(options["to_str"], ty.Callable):
+            raise Exception("iter_to_str.InvalidStyleError.to_str")
+
+    # Combine `style` with `options`
+
+    if style_name is not None:
+        style = inherit(ITER_TO_STR__STYLES, style_name)
+
+    elif style_dict is not None:
+        style = inherit(ITER_TO_STR__STYLES, "base", overlay=style_dict)
+
+    else:
+        raise Exception("iter_to_str.ProgrammerError")
+
+    if "extends" in style:
+        style = inherit(
+            ITER_TO_STR__STYLES,
+            style["extends"],
+            overlay=style,
+            delete_extends_key=True,
+        )
+
+    if "extends" in options:
+        options = inherit(
+            ITER_TO_STR__STYLES,
+            options["extends"],
+            overlay=options,
+            delete_extends_key=True,
+        )
+
+    style = {
+        **style,
+        **options,
+    }
+
+    # Style the iterable `iter`
+
+    def each_str(i: ty.Any, x: ty.Any, c: ty.Iterable) -> str:
+        return "".join((
+            style["before_each"],
+            style["to_str"](i, x, c),
+            style["after_each"],
+        ))
+
+    def all_str() -> str:
+        return "".join((
+            style["before_all"],
+            style["between"].join(each_str(i, x, c) for (i, x) in enumerate(c)),
+            style["after_all"],
+        ))
+
+    return all_str()
