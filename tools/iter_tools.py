1a66f1632c781da5f06687c61ca5be073d94646a
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-05-15 09:22:33 -0400

    tools.iter_tools: rewrite

--- tools/iter_tools.py
+++ tools/iter_tools.py
@@ -1,26 +1,62 @@
 ############################################################
 
-import itertools
-from collections.abc import Iterable
+import typing as ty
+from itertools import chain
 
 ############################################################
 
 
-def is_iterable(thing):
-
-    return isinstance(thing, Iterable)
-
-
-def list_join(*iters):
-
-    return list(itertools.chain(*iters))
+def is_iterable(thing) -> bool:
+    return isinstance(thing, ty.Iterable)
 
 
-def tuple_join(*iters):
-
-    return tuple(itertools.chain(*iters))
+############################################################
 
 
-def list_of_keys(some_dict):
+def join_as(
+    cast: ty.Callable[[ty.Iterable], ty.Iterable],
+    *iters: ty.Iterable[ty.Iterable],
+) -> ty.Iterable:
 
-    return list(some_dict.keys())
+    return cast(chain(*iters))
+
+
+def join_as_list(*iters: ty.Iterable[ty.Iterable]) -> list:
+
+    return join_as(list, *iters)
+
+
+def join_as_tuple(*iters: ty.Iterable[ty.Iterable]) -> tuple:
+
+    return join_as(tuple, *iters)
+
+
+def join_as_set(*iters: ty.Iterable[ty.Iterable]) -> set:
+
+    return join_as(set, *iters)
+
+
+############################################################
+
+
+def keys_as(
+    cast: ty.Callable[[ty.Iterable], ty.Iterable],
+    mapping: ty.Mapping,
+) -> ty.Iterable:
+
+    return cast(mapping.keys())
+
+
+def keys_as_list(mapping: ty.Mapping) -> list:
+
+    return keys_as(list, mapping)
+
+
+def keys_as_tuple(mapping: ty.Mapping) -> tuple:
+
+    return keys_as(tuple, mapping)
+
+
+def keys_as_set(mapping: ty.Mapping) -> set:
+
+    return keys_as(set, mapping)
