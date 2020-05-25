4496df210d1e427546709c15dcf6318a343312ba
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-05-24 21:23:48 -0400

    ancestor: create graph

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,2 +1,27 @@
-def earliest_ancestor(ancestors, starting_node):
-    pass
+############################################################
+
+from itertools import chain
+
+from projects.graph.graph import Graph
+
+############################################################
+
+
+def earliest_ancestor(ancestor_edges, from_node):
+
+    # Define `ancestors` graph from `ancestor_edges`...
+    ancestors = Graph()
+
+    # Define nodes:
+    ancestor_nodes = set(chain.from_iterable(ancestor_edges))
+    for node in ancestor_nodes:
+        ancestors.add_node(node)
+
+    # Define edges:
+    for parent, child in ancestor_edges:
+        # child -> parent
+        ancestors.add_edge(parent, child)
+
+    print(ancestors.get_map())
+
+    return
